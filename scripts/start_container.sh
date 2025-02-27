aa5cfb94b0e9e09e03079f6bf8e3068902be199b
Pitchika Tanmayi <63303908+Tanmayi5@users.noreply.github.com>
2025-02-27 20:38:51 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -8,3 +8,5 @@ docker pull tannnvvviiii5/sample-python-app
 # Run the Docker image as a container
 echo "Hiii Starting it"
 docker run -d -p 5000:5000 tannnvvviiii5/sample-python-app
+
+echo "Finsih"
