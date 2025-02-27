f168ea6df78a31f657c08583fcebf7e57631792d
Pitchika Tanmayi <63303908+Tanmayi5@users.noreply.github.com>
2025-02-27 20:17:25 +0530

    Appspec updated to root dir

---
+++ scripts/start_container.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+echo "Pulling the docker image"
+docker pull tannnvvviiii5/sample-python-app
+
+# Run the Docker image as a container
+echo "Hiii Starting it"
+docker run -d -p 5000:5000 tannnvvviiii5/sample-python-app
